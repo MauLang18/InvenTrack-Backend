0acdbb0c079b0815fabbe365660cef35e6d54b67
MauLang18 <maulangbonilla.18@gmail.com>
2024-08-08 13:30:57 -0600

    test: add unit test

---
+++ InvenTrackCore.Tests/Inventory/Commands/InventoryCommandsTest.cs
@@ -0,0 +1,103 @@
+﻿using InvenTrackCore.Application.Commons.Bases;
+using InvenTrackCore.Application.Commons.Exceptions;
+using InvenTrackCore.Application.UseCases.Inventory.Commands.CreateCommand;
+using InvenTrackCore.Application.UseCases.Inventory.Commands.UpdateCommand;
+using MediatR;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InvenTrackCore.Tests.Inventory.Commands;
+
+[TestClass]
+public class InventoryCommandsTest
+{
+    private static WebApplicationFactory<Program> _factory = null!;
+    private static IServiceScopeFactory _scopeFactory = null!;
+
+    [ClassInitialize]
+    public static void ClassInitialize(TestContext _)
+    {
+        _factory = new CustomWebApplicationFactory();
+        _scopeFactory = _factory.Services.GetRequiredService<IServiceScopeFactory>();
+    }
+
+    [TestMethod]
+    public async Task ShouldGetValidationErrors()
+    {
+        using var scope = _scopeFactory.CreateScope();
+        var mediator = scope.ServiceProvider.GetRequiredService<ISender>();
+
+        var command = new CreateInventoryCommand()
+        {
+            EquipmentTypeId = 1,
+            Brand = "",
+            Series = "",
+            Model = "",
+            Details = "",
+            Price = 12000,
+            State = 1
+        };
+
+        var expected = false;
+        BaseResponse<bool> response = new();
+
+        try
+        {
+            response = await mediator.Send(command);
+            Assert.Fail(response.Message);
+        }
+        catch (ValidationException ex)
+        {
+            Assert.IsNotNull(ex.Errors);
+            Assert.AreEqual(expected, response.IsSuccess);
+        }
+    }
+
+    [TestMethod]
+    public async Task ShouldCreateInventory()
+    {
+        using var scope = _scopeFactory.CreateScope();
+        var mediator = scope.ServiceProvider.GetRequiredService<ISender>();
+
+        var command = new CreateInventoryCommand()
+        {
+            EquipmentTypeId = 1,
+            Brand = "Laptpp",
+            Series = "L001",
+            Model = "Laptp",
+            Details = "",
+            Price = 12000,
+            State = 1
+        };
+
+        var expected = true;
+
+        var response = await mediator.Send(command);
+
+        Assert.AreEqual(expected, response.IsSuccess);
+    }
+
+    [TestMethod]
+    public async Task ShouldUpdateInventory()
+    {
+        using var scope = _scopeFactory.CreateScope();
+        var mediator = scope.ServiceProvider.GetRequiredService<ISender>();
+
+        var command = new UpdateInventoryCommand()
+        {
+            EquipmentTypeId = 2,
+            Brand = "Monitor",
+            Series = "M0001",
+            Model = "Monitor",
+            Details = "",
+            Price = 12000,
+            State = 1
+        };
+
+        var expected = true;
+
+        var response = await mediator.Send(command);
+
+        Assert.AreEqual(expected, response.IsSuccess);
+    }
+}
