c06d5f862ca4eda6e1d6366f83be82d241d19d6e
MauLang18 <maulangbonilla.18@gmail.com>
2024-08-08 13:41:52 -0600

    fix: change inventoryId

--- InvenTrackCore.Infrastructure/Persistence/Context/Configurations/InventaryConfiguration.cs
+++ InvenTrackCore.Infrastructure/Persistence/Context/Configurations/InventaryConfiguration.cs
@@ -10,7 +10,7 @@ public class InventaryConfiguration : IEntityTypeConfiguration<Inventory>
     {
         builder.HasKey(x => x.Id);
         builder.Property(x => x.Id)
-            .HasColumnName("InvetoryId");
+            .HasColumnName("InventoryId");
         builder.Property(x => x.Code)
             .IsUnicode();
         builder.Property(x => x.Active)
